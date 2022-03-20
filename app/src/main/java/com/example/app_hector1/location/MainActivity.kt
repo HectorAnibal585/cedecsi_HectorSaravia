5cb45179f5f2392304024ec0965571b1968816fb
HectorAnibal585 <hsaravia585@gmail.com>
2022-03-20 15:51:31 -0500

    Primer Commit

---
+++ app/src/main/java/com/example/app_hector1/location/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.app_hector1.location
+
+import android.Manifest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.app_hector1.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val LOG_TAG = "MainActivity"
+    }
+
+    private var txtLatitud: TextView? = null
+    private var txtLongitud: TextView? = null
+    private var btnCoordenadas: Button? = null
+    private lateinit var gpsProvider: GPSProvider
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        gpsProvider = GPSProvider(this)
+        setupViews()
+        setupListeners()
+    }
+
+    private fun setupViews() {
+        txtLatitud = findViewById(R.id.txtLatitud)
+        txtLongitud = findViewById(R.id.txtLongitud)
+        btnCoordenadas = findViewById(R.id.btnCoordenadas)
+    }
+
+    private fun setupListeners() {
+        btnCoordenadas?.setOnClickListener {
+            gpsProvider.checkPermission()
+        }
+        gpsProvider.onLocation = {
+            txtLatitud?.text = "Latitud: ${it.latitude}"
+            txtLongitud?.text = "Latitud: ${it.longitude}"
+        }
+    }
+
+    override fun onDestroy() {
+        gpsProvider.onDestroy()
+        super.onDestroy()
+    }
+
+}
+
